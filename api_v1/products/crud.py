d649fc38acbfb6aae39fed4d45c07f78eb245c7f
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2024-12-01 11:35:46 +0500

    function for create new product in db

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -16,3 +16,12 @@ async def get_products(session: AsyncSession) -> list[Product]:
     result: Result = await session.execute(stnt)
     products = []
     return products
+
+async def get_product(session: AsyncSession, product_id: int) -> Product | None:
+    return await session.get(Product, product_id)
+
+
+async def create_product(session: AsyncSession, product_in: ProductCreate) -> Product:
+    product = Product(**product_in.model_dump())
+    session.add(product)
+    await session.commit()
