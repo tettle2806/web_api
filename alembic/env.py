67bb94c78ba70140288053d5f45894defac77ab9
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2024-12-05 21:10:24 +0500

    first migrations

--- alembic/env.py
+++ alembic/env.py
@@ -20,7 +20,11 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from core.models import Base
+from core.config import settings
+
+config.set_main_option("sqlalchemy.url", settings.db_url)
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
