b1677a681702137c984b241eec3bb8afb862704f
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2024-12-10 22:37:54 +0500

    create user relation mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,23 +1,24 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, relationship
-from sqlalchemy.testing.schema import mapped_column, Column
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins import UserRelationMixin
 
-if TYPE_CHECKING:
-    from .user import User
 
-class Post(Base):
-    title: Mapped[String] = mapped_column(String(100), unique=False)
-    body: Mapped[String] = mapped_column(
+class Post(UserRelationMixin, Base):
+    # _user_id_nullable = False
+    # _user_id_unique = False
+    _user_back_populates = "posts"
+
+    title: Mapped[str] = mapped_column(String(100), unique=False)
+    body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-        # nullable=False,
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.title!r}, user_id={self.user_id})"
+
+    def __repr__(self):
+        return str(self)
