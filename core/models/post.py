99e5bb8c187b8c7885ca6976055d874cc40c299a
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2024-12-10 21:58:34 +0500

    create relationships for posts and users'

--- core/models/post.py
+++ core/models/post.py
@@ -1,9 +1,13 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.testing.schema import mapped_column, Column
 
 from .base import Base
 
+if TYPE_CHECKING:
+    from .user import User
 
 class Post(Base):
     title: Mapped[String] = mapped_column(String(100), unique=False)
@@ -13,6 +17,7 @@ class Post(Base):
         server_default="",
     )
     user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id", ondelete="CASCADE"),
-
+        ForeignKey("users.id"),
+        # nullable=False,
     )
+    user: Mapped["User"] = relationship(back_populates="posts")
--- core/models/user.py
+++ core/models/user.py
@@ -1,9 +1,14 @@
+from typing import List, TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.testing.schema import mapped_column
 
 from .base import Base
 
+if TYPE_CHECKING:
+    from .post import Post
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
+    posts: Mapped[List["Post"]] = relationship(back_populates="user")
