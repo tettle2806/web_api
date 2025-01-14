a742802440ac4782f282f2cbc9800592c7e59ec9
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-01-14 16:10:53 +0500

    order finished

--- core/models/order.py
+++ core/models/order.py
@@ -1,21 +1,28 @@
-from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from .base import Base
 from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
 
 if TYPE_CHECKING:
     from .product import Product
+    from .order_product_association import OrderProductAssociation
 
 
 class Order(Base):
     promocode: Mapped[str | None]
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.now(),
-        default=datetime.utcnow(),
+        default=datetime.utcnow,
     )
     products: Mapped[list["Product"]] = relationship(
-        back_populates="orders",
         secondary="order_product_association",
+        back_populates="orders",
+    )
+
+    # association between Parent -> Association -> Child
+    products_details: Mapped[list["OrderProductAssociation"]] = relationship(
+        back_populates="order"
     )
