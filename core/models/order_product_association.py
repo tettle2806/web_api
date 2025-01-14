c7384b016905796a820a00d35af2d9c7d0451ffa
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-01-14 16:11:30 +0500

    orm for association model

--- core/models/order_product_association.py
+++ core/models/order_product_association.py
@@ -1,8 +1,13 @@
-from sqlalchemy import Table, Column, ForeignKey, Integer, UniqueConstraint
-from sqlalchemy.orm import Mapper, Mapped
-from sqlalchemy.testing.schema import mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .order import Order
+    from .product import Product
 
 
 class OrderProductAssociation(Base):
@@ -19,4 +24,13 @@ class OrderProductAssociation(Base):
     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
     count: Mapped[int] = mapped_column(default=1, server_default="1")
-    # unit_price: Mapped[int] = mapped_column(default=1)
+    unit_price: Mapped[int] = mapped_column(default=0, server_default="0")
+
+    # association between Assocation -> Order
+    order: Mapped["Order"] = relationship(
+        back_populates="products_details",
+    )
+    # association between Assocation -> Product
+    product: Mapped["Product"] = relationship(
+        back_populates="orders_details",
+    )
