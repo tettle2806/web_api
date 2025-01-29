7bdaee7a48c6a1e92c9b5eba8b4fe99dbf591327
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-01-29 09:04:13 +0500

    avg price function

--- blockchain_endpoints/views.py
+++ blockchain_endpoints/views.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
-from blockchains_api.bitcoin_api import binance_ticker_prices
+from blockchains_api.bitcoin_api import binance_ticker_price, binance_avg_price
 
 router = APIRouter(
     prefix="/crypto",
@@ -9,7 +9,12 @@ router = APIRouter(
 )
 
 
-@router.get("price")
+@router.get("/price")
 async def crypto_price(ticker: str)-> JSONResponse:
-    logs = await binance_ticker_prices(ticker)
+    logs = await binance_ticker_price(ticker)
+    return logs
+
+@router.get("/avg_price")
+async def avg_price(ticker: str)-> JSONResponse:
+    logs = await binance_ticker_price(ticker)
     return logs
--- blockchains_api/bitcoin_api.py
+++ blockchains_api/bitcoin_api.py
@@ -1,32 +1,17 @@
 import requests
 
 
-async def binance_ticker_prices(ticker: str) -> dict:
+async def binance_ticker_price(ticker: str) -> dict:
     r = requests.get(f'https://data-api.binance.vision/api/v3/ticker/price?symbol={ticker}')
+    return r.json()
+
+async def binance_avg_price(ticker: str) -> dict:
+    r = requests.get(f'https://data-api.binance.vision/api/v3/avgPrice?symbol={ticker}')
     return r.json()
 
 
 
 
-# # Simple python websocket client
-# # https://github.com/websocket-client/websocket-client
-# from core.config import api_key, token
-# from websocket import create_connection
-#
-# options = {}
-# print(token)
-# options['origin'] = 'https://exchange.blockchain.com'
-# url = "wss://ws.blockchain.info/mercury-gateway/v1/ws"
-# ws = create_connection(url, **options)
-# msg = '{"token": "{eyJhbGciOiJFUzI1NiIsInR5cCI6IkFQSSJ9.eyJpc3MiOiJibG9ja2NoYWluIiwiYXVkIjoibWVyY3VyeSIsImlhdCI6MTczODA2MTY5NiwianRpIjoiMWZiODIwMzUtNmZiNC00YjYxLTg5NmMtMjJiYjYzMWQxOGU0IiwidWlkIjoiZWZkOTM2NWUtYWQ5NS00YzdmLWI2MmUtNDA4YTM3ZTRhZWUxIiwic2VxIjo4MjQwNTk2LCJyZG8iOnRydWUsIndkbCI6ZmFsc2V9.IB/+pDzQCV+81Ao3vxJBEYw39OgORzhDxIDi7wSzPXlCXbya3N906fwAxZ51HDzJxBqyKwCJmrmbolw9EXR1YKc=}", "action": "subscribe", "channel": "prices", "symbol": "BTC-USD", "granularity": 60}'
-# ws.send(msg)
-# result =  ws.recv()
-# print(result)
-# # { "seqnum":0,
-# #   "event":"subscribed",
-# #   "channel":"auth",
-# #   "readOnly":false }
-# ws.close()
 
 
 
