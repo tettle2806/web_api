e51bcca5a08a68e8ce9cbb38437fe6f916106d51
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-01-29 08:12:55 +0500

    add dotenv function

--- blockchains_api/bitcoin_api.py
+++ blockchains_api/bitcoin_api.py
@@ -1,12 +1,13 @@
 # Simple python websocket client
 # https://github.com/websocket-client/websocket-client
+from core.config import api_key, token
 from websocket import create_connection
 options = {}
-
+print(token)
 options['origin'] = 'https://exchange.blockchain.com'
 url = "wss://ws.blockchain.info/mercury-gateway/v1/ws"
 ws = create_connection(url, **options)
-msg = '{"token": "{API_SECRET}", "action": "subscribe", "channel": "auth"}'
+msg = '{"token": "{eyJhbGciOiJFUzI1NiIsInR5cCI6IkFQSSJ9.eyJpc3MiOiJibG9ja2NoYWluIiwiYXVkIjoibWVyY3VyeSIsImlhdCI6MTczODA2MTY5NiwianRpIjoiMWZiODIwMzUtNmZiNC00YjYxLTg5NmMtMjJiYjYzMWQxOGU0IiwidWlkIjoiZWZkOTM2NWUtYWQ5NS00YzdmLWI2MmUtNDA4YTM3ZTRhZWUxIiwic2VxIjo4MjQwNTk2LCJyZG8iOnRydWUsIndkbCI6ZmFsc2V9.IB/+pDzQCV+81Ao3vxJBEYw39OgORzhDxIDi7wSzPXlCXbya3N906fwAxZ51HDzJxBqyKwCJmrmbolw9EXR1YKc=}", "action": "subscribe", "channel": "prices", "symbol": "BTC-USD", "granularity": 60}'
 ws.send(msg)
 result =  ws.recv()
 print(result)
--- core/config.py
+++ core/config.py
@@ -4,6 +4,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+token = os.getenv("TOKEN")
+api_key = os.getenv("API_KEY")
+
 BASE_DIR = Path(__file__).parent.parent
 DB_PATH = BASE_DIR / "db.sqlite3"
 
