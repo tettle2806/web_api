df745cfe2ac9ca99e01c84343036c1d8f10d9573
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-01-31 10:12:47 +0500

    create pydantic models for jwt auth

--- users/schemas.py
+++ users/schemas.py
@@ -8,3 +8,22 @@ class CreateUser(BaseModel):
     username: Annotated[str, MinLen(3), MaxLen(20)]
     email: EmailStr
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
