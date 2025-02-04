d790a514d2f79012223ce16bc9c1bd53d2d30d44
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-04 15:04:08 +0500

    added uuid to the jwt token encoding

--- users/utils.py
+++ users/utils.py
@@ -1,6 +1,7 @@
 import hashlib
 from datetime import datetime, timedelta
 from typing import Union, Any
+from uuid import UUID
 
 import jwt
 from passlib.context import CryptContext
@@ -26,7 +27,9 @@ def verify_password(password: str, hashed_pass: str) -> bool:
     return False
 
 
-def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+def create_access_token(
+    uuid: UUID, subject: Union[str, Any], expires_delta: int = None
+) -> str:
     if expires_delta is not None:
         expires_delta = datetime.utcnow() + expires_delta
     else:
@@ -34,12 +37,14 @@ def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> 
             minutes=ACCESS_TOKEN_EXPIRE_MINUTES
         )
 
-    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    to_encode = {"exp": expires_delta, "sub": str(subject), "uuid": str(uuid)}
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
     return encoded_jwt
 
 
-def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+def create_refresh_token(
+    uuid: UUID, subject: Union[str, Any], expires_delta: int = None
+) -> str:
     if expires_delta is not None:
         expires_delta = datetime.utcnow() + expires_delta
     else:
@@ -47,6 +52,6 @@ def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) ->
             minutes=REFRESH_TOKEN_EXPIRE_MINUTES
         )
 
-    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    to_encode = {"exp": expires_delta, "sub": str(subject), "uuid": str(uuid)}
     encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
     return encoded_jwt
