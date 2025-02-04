51cab4cf9f5d78283d11e3b5b27ee44c04597f89
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-04 15:02:56 +0500

    moved authorization from the users directory

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ import sys
 from contextlib import asynccontextmanager
 
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from starlette.responses import RedirectResponse
 
 from api_v1 import router as router_v1
 import uvicorn
 from core.models import Base, db_helper
 from items_views import router as items_router
+from users.crud import get_user_by_username
+from users.schemas import TokenSchema
+from users.utils import (
+    get_hashed_password,
+    verify_password,
+    create_access_token,
+    create_refresh_token,
+)
 from users.views import router as users_router
 from core.config import settings
 
@@ -40,6 +49,34 @@ app.include_router(router=blockchain_router)
 @app.get("/")
 def hello_index():
     return RedirectResponse(url="/docs")
+
+
+@app.post(
+    "/login",
+    summary="Create access and refresh tokens for user",
+    response_model=TokenSchema,
+)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    async with db_helper.session_factory() as session:
+        user = await get_user_by_username(session=session, username=form_data.username)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password",
+        )
+
+    hashed_pass = user.password
+    db_hash_password = get_hashed_password(form_data.password)
+    if not verify_password(db_hash_password, hashed_pass):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password",
+        )
+
+    return {
+        "access_token": create_access_token(uuid=user.uuid, subject=user.email),
+        "refresh_token": create_refresh_token(uuid=user.uuid, subject=user.email),
+    }
 
 
 if __name__ == "__main__":
