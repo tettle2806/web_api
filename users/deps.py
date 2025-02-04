fd7c33c2d2ce3528ce8c35a25d25814ccf5be666
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-04 15:02:22 +0500

    removed duplicate code and fixed bugs

--- users/deps.py
+++ users/deps.py
@@ -1,45 +1,42 @@
-from typing import Union, Any
+import asyncio
 from datetime import datetime
 
 import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
 
-from .crud import get_user_by_email
+from core.models import db_helper
+from users.crud import get_user_by_email
 from .utils import ALGORITHM, JWT_SECRET_KEY
 
-from pydantic import ValidationError
 from users.schemas import TokenPayload, SystemUser
 
 reuseable_oauth = OAuth2PasswordBearer(tokenUrl="/login", scheme_name="JWT")
 
 
-async def get_current_user(token: str = Depends(reuseable_oauth)) -> SystemUser:
-    print(token)
+async def get_current_user(token: str = Depends(reuseable_oauth)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
     try:
         payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
         token_data = TokenPayload(**payload)
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
 
         if datetime.fromtimestamp(token_data.exp) < datetime.now():
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Token expired",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-    except (jwt.JWTError, ValidationError):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+            raise credentials_exception
 
-    user: Union[dict[str, Any], None] = await get_user_by_email(token_data.email)
-
+    except InvalidTokenError:
+        raise
+    async with db_helper.session_factory() as session:
+        user = await get_user_by_email(session=session, email=email)
     if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Could not find user",
-        )
-    print(user)
-    return SystemUser(**user)
-
+        return credentials_exception
+    return user
+    # return SystemUser(uuid=user.uuid, email=user.email, password=user.password)
