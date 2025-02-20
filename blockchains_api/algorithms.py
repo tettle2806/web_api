67c846953bdf5fd59fcfd012aebda04f472b2200
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-20 17:35:42 +0500

    added data to check how the function works

--- blockchains_api/algorithms.py
+++ blockchains_api/algorithms.py
@@ -7,10 +7,9 @@
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
+from backtesting import Strategy, Backtest
+from pygments.lexers import go
 from tqdm import tqdm
-import plotly.graph_objects as go
-from backtesting import Strategy
-from backtesting import Backtest
 
 df = pd.read_csv("EURUSD_Candlestick_5_M_ASK_30.09.2019-30.09.2022.csv")
 
